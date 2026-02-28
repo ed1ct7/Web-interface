2dce737c34c8e6f4a2605d046650501ad90efc9f
ed1ct7 <ediktigranian@gmail.com>
2026-02-28 18:21:41 +0300

    refactor: redesign general component v.1

--- web-interface/src/Pages/Base/General.jsx
+++ web-interface/src/Pages/Base/General.jsx
@@ -8,47 +8,51 @@ function General() {
         <div className="base">
             <form className="base__card">
                 <h1 className="base__header">Меню</h1>
-                <div className="general__grid">
-                    <div className="general__labels">
-                        <label className="general__label">Имя</label>
-                        <label className="general__label">Почта</label>
-                        <label className="general__label">Пароль</label>
-                        <label className="general__label">Группа</label>
+
+                <div className="general">
+                    <div className="general__grid">
+
+                        <div className="general__label">Имя</div>
+                        <div>
+                            <span className="general__value">Антон Савельев</span>
+                            <div className="general__line" />
+                        </div>
+
+                        <div className="general__label">Почта</div>
+                        <div>
+                          <span className="general__value">
+                            thenorthwizards@gmail.com
+                          </span>
+                            <div className="general__line" />
+                        </div>
+
+                        <div className="general__label">Пароль</div>
+                        <div>
+                            <span className="general__value">Password222</span>
+                            <div className="general__line" />
+                        </div>
+
+                        <div className="general__label">Группа</div>
+                        <div>
+                            <span className="general__value">C326</span>
+                            <div className="general__line" />
+                        </div>
+
                     </div>
 
-                    <div className="general__textboxes">
-                        <div className="general__row">
-                            <label className="general__labelData">Антон Савельев</label>
-                            <div className="general__line" />
-                        </div>
-                        <div className="general__row">
-                            <label className="general__labelData">thenorthwizards@gmail.com</label>
-                            <div className="general__line" />
-                        </div>
-                        <div className="general__row">
-                            <label className="general__labelData">Password222</label>
-                            <div className="general__line" />
-                        </div>
-                        <div className="general__row">
-                            <label className="general__labelData">C326</label>
-                            <div className="general__line" />
-                        </div>
+                    <div className="general__actions">
+                        <button type="button" className="general__actionBtn">
+                            <img src={settingsIcon} alt="" className="general__icon" />
+                        </button>
 
+                        <button type="button" className="general__actionBtn">
+                            <img src={folderIcon} alt="" className="general__icon" />
+                        </button>
+
+                        <button type="button" className="general__actionBtn">
+                            <img src={exitIcon} alt="" className="general__icon" />
+                        </button>
                     </div>
-                </div>
-
-                <div className="general__actions">
-                    <button type="button" className="general__actionBtn">
-                        <img src={settingsIcon} alt="" className="general__icon" />
-                    </button>
-
-                    <button type="button" className="general__actionBtn">
-                        <img src={folderIcon} alt="" className="general__icon" />
-                    </button>
-
-                    <button type="button" className="general__actionBtn">
-                        <img src={exitIcon} alt="" className="general__icon" />
-                    </button>
                 </div>
             </form>
         </div>
