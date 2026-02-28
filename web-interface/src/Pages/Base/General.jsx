5989f39f4c37616b6e9d005ae2f00e412abb4206
ed1ct7 <ediktigranian@gmail.com>
2026-02-28 17:41:10 +0300

    feat: enchance general component

--- web-interface/src/Pages/Base/General.jsx
+++ web-interface/src/Pages/Base/General.jsx
@@ -1,15 +1,13 @@
 import "./General.scss";
+import exitIcon from "../../Assets/exit.svg";
+import folderIcon from "../../Assets/folder.svg";
+import settingsIcon from "../../Assets/settings.svg";
 
 function General() {
     return (
         <div className="base">
-            <form className="base__card general">
+            <form className="base__card">
                 <h1 className="base__header">Меню</h1>
-
-                <button type="button" className="general__open">
-                    Открыть папку
-                </button>
-
                 <div className="general__grid">
                     <div className="general__labels">
                         <label className="general__label">Имя</label>
@@ -18,20 +16,38 @@ function General() {
                         <label className="general__label">Группа</label>
                     </div>
 
-                    <div className="general__values">
-                        <div className="general__line" />
-                        <div className="general__line" />
-                        <div className="general__line" />
-                        <div className="general__line" />
+                    <div className="general__textboxes">
+                        <div className="general__row">
+                            <label className="general__labelData">Антон Савельев</label>
+                            <div className="general__line" />
+                        </div>
+                        <div className="general__row">
+                            <label className="general__labelData">thenorthwizards@gmail.com</label>
+                            <div className="general__line" />
+                        </div>
+                        <div className="general__row">
+                            <label className="general__labelData">Password222</label>
+                            <div className="general__line" />
+                        </div>
+                        <div className="general__row">
+                            <label className="general__labelData">C326</label>
+                            <div className="general__line" />
+                        </div>
+
                     </div>
                 </div>
 
                 <div className="general__actions">
-                    <button type="button" className="base__button">
-                        Изм
+                    <button type="button" className="general__actionBtn">
+                        <img src={settingsIcon} alt="" className="general__icon" />
                     </button>
-                    <button type="button" className="base__button">
-                        Выйти
+
+                    <button type="button" className="general__actionBtn">
+                        <img src={folderIcon} alt="" className="general__icon" />
+                    </button>
+
+                    <button type="button" className="general__actionBtn">
+                        <img src={exitIcon} alt="" className="general__icon" />
                     </button>
                 </div>
             </form>
